e3992d4de2ed7f4ff1b01be4092787324ef95cb7
FLLAYoooo <mat38.rlo@gmail.com>
2022-11-10 10:10:02 +0100

    Final commit

--- morpion.py
+++ morpion.py
@@ -1,52 +1,118 @@
-""" import de la fonction random"""
+"""
+Module Doc
+"""
+
+####################
+# Libraries import #
+####################
+
+# We import the randint item frop random librarie so we can generate random numbers
 from random import randint
 
+########################
+# Functions definition #
+########################
 
-cases={"a1":[" ","l"],"b1":[" ","l"],"c1":[" ","l"],"a2":[" ","l"],"b2":[" ","l"],"c2":[" ","l"],"a3":[" ","l"],"b3":[" ","l"],"c3":[" ","l"]}
 
-print("  ___A__ ___B__ ___C__\n |      |      |      |\n1|   "+cases["a1"][0]+"  |   "+cases["b1"][0]+"  |   "+cases["c1"][0]+"  |\n |______|______|______|\n |      |      |      |\n2|   "+cases["a2"][0]+"  |   "+cases["b2"][0]+"  |   "+cases["b2"][0]+"  |\n |______|______|______|\n |      |      |      |\n3|   "+cases["a3"][0]+"  |   "+cases["b3"][0]+"  |   "+cases["c3"][0]+"  |\n |______|______|______|")
+board={"a1":[" ","l"],"b1":[" ","l"],"c1":[" ","l"],"a2":[" ","l"],\
+    "b2":[" ","l"],"c2":[" ","l"],"a3":[" ","l"],"b3":[" ","l"],"c3":[" ","l"]}
 
-def jeux():
-    """Jeux de morpion
-    <-- rien
-    --> rien """
-    victoire = False
-    nbtours = 0
-    aqui = randint(1,2)
-    case = "None"
-    while not victoire and nbtours<9:
-        print("c'est au tour du joueur",aqui,"de jouer ! ")
-        while case not in cases or cases[case][1] != "l":
-            case = str(input("Sur quel case voulez vous jouer? "))
-        if aqui == 1:
-            cases[case][0] = "X"
+print("  ___A__ ___B__ ___C__\n |      |      |      |\n1|   " +
+board["a1"][0] +
+"  |   " +
+board["b1"][0] +
+"  |   "+board["c1"][0] +
+"  |\n |______|______|______|\n |      |      |      |\n2|   " +
+board["a2"][0] +
+"  |   " +
+board["b2"][0] +
+"  |   " +
+board["b2"][0] +
+"  |\n |______|______|______|\n |      |      |      |\n3|   " +
+board["a3"][0] +
+"  |   " +
+board["b3"][0] +
+"  |   " +
+board["c3"][0] +
+"  |\n |______|______|______|")
+
+def game():
+    """Morpion game
+    <-- nothing
+    --> nothing """
+    victory = False
+    turnsnumber = 0
+    who = randint(1,2)
+    square = "None"
+    while not victory and turnsnumber<9:
+        print("c'est au tour du joueur",who,"de jouer ! ")
+        while square not in board or board[square][1] != "l":
+            square = str(input("Sur quel square voulez vous jouer? ")).lower()
+        if who == 1:
+            board[square][0] = "X"
         else:
-            cases[case][0] = "O"
-        cases[case][1] = "o"
+            board[square][0] = "O"
+        board[square][1] = "o"
 
-        print("  ___A__ ___B__ ___C__\n |      |      |      |\n1|   "+cases["a1"][0]+"  |   "+cases["b1"][0]+"  |   "+cases["c1"][0]+"  |\n |______|______|______|\n |      |      |      |\n2|   "+cases["a2"][0]+"  |   "+cases["b2"][0]+"  |   "+cases["c2"][0]+"  |\n |______|______|______|\n |      |      |      |\n3|   "+cases["a3"][0]+"  |   "+cases["b3"][0]+"  |   "+cases["c3"][0]+"  |\n |______|______|______|")
-        victoire = victory()
-        if aqui == 1:
-            aqui = 2
+        print("  ___A__ ___B__ ___C__\n |      |      |      |\n1|   " +
+        board["a1"][0] +
+        "  |   " +
+        board["b1"][0] +
+        "  |   " +
+        board["c1"][0] +
+        "  |\n |______|______|______|\n |      |      |      |\n2|   " +
+        board["a2"][0] +
+        "  |   " +
+        board["b2"][0] +
+        "  |   " +
+        board["c2"][0] +
+        "  |\n |______|______|______|\n |      |      |      |\n3|   " +
+        board["a3"][0] +
+        "  |   " +
+        board["b3"][0] +
+        "  |   " +
+        board["c3"][0] +
+        "  |\n |______|______|______|")
+        victory = gamewin()
+        if who == 1:
+            who = 2
         else:
-            aqui = 1
-        nbtours += 1
-    if victoire:
-        print("c'est le joueur",aqui,"qui a gagné la partie")
+            who = 1
+        turnsnumber += 1
+    if victory:
+        print("C'est le joueur",who,"qui a gagné la partie")
     else:
-        print("égalitée")
+        print("Egalitée")
 
 
-def victory():
-    """ Condition de victoire du morpion
-    <-- rien
-    --> bool gagné ou pas """
-    gagne = False
-    if cases["a1"][1] ==  cases["a2"][1] ==  cases["a3"][1] ==  cases["b1"][1] ==  cases["b2"][1] == cases["b3"][1] == "o":
-        print("\n██████████████████\n█░░░░░░░░░░░░░░░░█\n█░██████░░██████░█\n█░█░░░░█░░█░░░░█░█\n█░█░░░░█░░█░░░░█░█\n█░██████░░██████░█\n█░░░░░░░░░░░░░░░░█\n█░██████░░██████░█\n█░█░░░░█░░█░░░░█░█\n█░█░░░░█░░█░░░░█░█\n█░██████░░██████░█\n█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n██████████████████\n\n Vous avez crafté une porte\n")
-    if cases["a1"][0] == cases["b1"][0] == cases["c1"][0] != " " or cases["a1"][0] == cases["b1"][0] == cases["c1"][0] != " " or cases["a3"][0] == cases["b3"][0] == cases["c3"][0] != " " or cases["a1"][0] == cases["a2"][0] == cases["a3"][0] != " " or cases["b1"][0] == cases["b2"][0] == cases["b3"][0] != " " or cases["c1"][0] == cases["c2"][0] == cases["c3"][0] != " "  or cases["a1"][0] == cases["b2"][0] == cases["c3"][0] != " " or cases["a3"][0] == cases["b2"][0] == cases["c1"][0] != " ":
-        gagne = True
-    return gagne
+def gamewin():
+    """ Win conditions
+    <-- nothing
+    --> bool win or no"""
+    win = False
+    if board["a1"][1] ==  board["a2"][1] ==  board["a3"][1] ==  board["b1"]\
+        [1] ==  board["b2"][1] == board["b3"][1] == "o" \
+        and board["c1"][1] ==  board["c2"][1] == board["c3"][1] == "l":
+        print("\n██████████████████\n█░░░░░░░░░░░░░░░░█\n█░██████░░██████░█\n" + \
+        "█░█░░░░█░░█░░░░█░█\n█░█░░░░█░░█░░░░█░█\n█░██████░░██████░█\n" + \
+        "█░░░░░░░░░░░░░░░░█\n█░██████░░██████░█\n█░█░░░░█░░█░░░░█░█\n" + \
+        "█░█░░░░█░░█░░░░█░█\n█░██████░░██████░█\n█░░░░░░░░░░░░░░░░█\n" + \
+        "█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n" + \
+        "█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░█\n" + \
+        "██████████████████\n\n Vous avez crafté une porte\n")
+    if board["a1"][0] == board["b1"][0] == board["c1"][0] != " " or board["a1"]\
+    [0] == board["b1"][0] == board["c1"][0] != " " or board["a3"][0] == board["b3"]\
+    [0] == board["c3"][0] != " " or board["a1"][0] == board["a2"][0] == board["a3"]\
+    [0] != " " or board["b1"][0] == board["b2"][0] == board["b3"]\
+    [0] != " " or board["c1"][0] == board["c2"][0] == board["c3"]\
+    [0] != " "  or board["a1"][0] == board["b2"]\
+    [0] == board["c3"][0] != " " or board["a3"]\
+    [0] == board["b2"][0] == board["c1"][0] != " ":
+        win = True
+    return win
 
+######################
+# Instructions chain #
+######################
 
-jeux()
+game()
